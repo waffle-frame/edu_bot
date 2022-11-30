831276518b96df350f24ebec29a69c821b50b774
nappy <Waffle>
2022-12-01 00:37:36 +0500

    change session name of the telethon connection

--- settings/userbot.py
+++ settings/userbot.py
@@ -6,7 +6,7 @@ from utils.config import Userbot
 # Authorization and session file generation
 async def setup_userbot(conf: Userbot) -> TelegramClient:
     client = TelegramClient(
-        f"session_{conf.phone_number}", conf.api_id, conf.api_hash
+        "session", conf.api_id, conf.api_hash
     )
     await client.connect()
 
