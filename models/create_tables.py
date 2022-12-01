0c74087899cf4f9fb6eefbccef22adea8922c81d
nappy <Waffle>
2022-12-01 23:54:35 +0500

    fixed function for creating tables

--- models/create_tables.py
+++ models/create_tables.py
@@ -2,7 +2,7 @@ import os, sys
 import asyncio
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+os.pardir)
 
-from models import Base
+from models.groups import Group
 from utils.config import load_config
 from settings.database import setup_database
 
@@ -11,7 +11,9 @@ async def create_table():
     conf = load_config().database
     engine, _= setup_database(conf)
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Group.__table__.create)
+
+    await engine.dispose()
 
 if __name__ == "__main__":
     asyncio.run(create_table())
