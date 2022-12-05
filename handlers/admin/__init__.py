ddf0f0afc68f3b5f61291fa0780e34ca8f27ddcb
nappy <Waffle>
2022-12-06 00:39:07 +0500

    protect search inline mode

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -10,7 +10,8 @@ from handlers.admin.search import inline_echo, get_similar_movies
 def register_admin_commands(dp: Dispatcher):
     admin_ids = load_config().admins.admin_ids
 
-    dp.register_inline_handler(inline_echo)
     dp.register_message_handler(tail_history, IDFilter(user_id=admin_ids), Command("history"), state="*")
     dp.register_message_handler(remove_user, IDFilter(user_id=admin_ids), Command("remove_user"), state="*")
-    dp.register_chosen_inline_handler(get_similar_movies, state="*")
+
+    dp.register_inline_handler(inline_echo, IDFilter(user_id=admin_ids))
+    dp.register_chosen_inline_handler(get_similar_movies, IDFilter(user_id=admin_ids), state="*")
